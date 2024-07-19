936f5a205d6cb3f4557af048cc95c0cc2453aad5
alankaiko <alamdfs@hotmail.com>
2024-07-19 00:03:53 -0300

    leitura basica

--- src/app/pagina-conversao/pagina-conversao.component.ts
+++ src/app/pagina-conversao/pagina-conversao.component.ts
@@ -6,8 +6,21 @@ import {Component, EventEmitter} from '@angular/core';
   styleUrls: ['./pagina-conversao.component.css']
 })
 export class PaginaConversaoComponent {
-  onUpload: EventEmitter<any> = new EventEmitter<any>();
-  onError: EventEmitter<any> = new EventEmitter<any>();
+  fileContent: string | ArrayBuffer | null = null;
 
+  selecionarArquivo(event: Event): void {
+    const arquivo = event.target as HTMLInputElement;
+    if (arquivo.files && arquivo.files.length) {
+      const file = arquivo.files[0];
+      this.conteudoArquivo(file);
+    }
+  }
 
+  conteudoArquivo(file: File): void {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      this.fileContent = reader.result;
+    };
+    reader.readAsText(file);
+  }
 }
